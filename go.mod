fbbf5782c0bdab39b2b73e2efd177e391ec380ee
Twinsiller <boldinov.leha@yandex.ru>
2024-12-09 00:23:47 +0300

    wow

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module ProjectONE
 go 1.23.1
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.10.0
 	github.com/joho/godotenv v1.5.1
 	github.com/sirupsen/logrus v1.9.3
@@ -13,7 +14,6 @@ require (
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
---
+++ internal/api/v1/middle.go
@@ -0,0 +1,97 @@
+package v1
+
+import (
+	database "ProjectONE/internal/database/postgres"
+	"ProjectONE/internal/models"
+	"ProjectONE/pkg/utils"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	password "github.com/vzglad-smerti/password_hash"
+)
+
+var jwtKey = []byte(os.Getenv("JWT_SECRET"))
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func generateToken(username string) (string, error) {
+	expirationTime := time.Now().Add(20 * time.Minute)
+	claims := &Claims{
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, _ := token.SignedString(jwtKey)
+	fmt.Println("\n\n", ss)
+	return token.SignedString(jwtKey)
+}
+
+func login(c *gin.Context) {
+	var creds Credentials
+	if err := c.BindJSON(&creds); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid request"})
+		return
+	}
+
+	row := database.DbPostgres.QueryRow("select nickname, hash_password from authors where nickname = $1", creds.Username)
+	pc := models.ProfileCheck{}
+	fmt.Println(row)
+	err := row.Scan(&pc.Nickname, &pc.HashPassword)
+	fmt.Println(pc.Nickname, pc.HashPassword)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "Bad check profile"})
+		return
+	}
+	if creds.Username != pc.Nickname {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "User doesn't exist"})
+		return
+	}
+	if ok, err := password.Verify(pc.HashPassword, creds.Password); ok == false || err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "Password error!!!"})
+		return
+	}
+
+	token, err := generateToken(creds.Username)
+	if err != nil {
+		utils.Logger.Error(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "could not create token"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": token})
+}
+
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := strings.Split(c.GetHeader("Authorization"), " ")
+		//fmt.Println("\n\n", tokenString)
+		claims := &Claims{}
+		//fmt.Println("\n\nclaims.Username ", claims.Username)
+		token, err := jwt.ParseWithClaims(tokenString[1], claims, func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "unauthorized"})
+			c.Abort()
+			return
+		}
+		//fmt.Printf("%v %v", claims.Username, claims.StandardClaims.ExpiresAt)
+		c.Next()
+	}
+}
